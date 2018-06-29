48aaf0b640c3770b5e7673a482251b36c494ad9a
Derek Peterson <derekepterson.dpp@gmail.com>
2018-06-28 21:07:39 -0400

    also draw numbers on image

--- draw_galaxy.py
+++ draw_galaxy.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw
 import json
 import os
 
@@ -6,6 +6,7 @@ TILE_IMAGES = {}
 TILE_DIR = "./res"
 TILE_IMAGE_X = 900
 TILE_IMAGE_Y = 774
+FONT_PATH = "./res/Slider Regular.ttf"
 
 
 def get_tile_image(number):
@@ -29,25 +30,42 @@ def get_tile_image(number):
         return None
     return TILE_IMAGES[number]
 
+def draw_number_at_coordinates(number, fnt, image, coords):
+    d.text(coords, str(number), font=fnt)
 
-def calc_coordinates(i, j, image_x, image_y, n_row_offset):
-    start_offset = n_row_offset * (image_y) / 2
-    out_x = (image_x * 3 / 4) * i
-    out_y = start_offset + j * image_y - i * (image_y/2)
+
+def calc_coordinates(i, j, n_row_offset):
+    start_offset = n_row_offset * (TILE_IMAGE_Y) / 2
+    out_x = (TILE_IMAGE_X * 3 / 4) * i
+    out_y = start_offset + j * TILE_IMAGE_Y - i * (TILE_IMAGE_Y/2)
     return (out_x, out_y)
+
+def calc_text_coords(i, j, n_row_offset):
+    coords = calc_coordinates(i, j, n_row_offset)
+    return (coords[0] + TILE_IMAGE_X / 8,
+            coords[1] + TILE_IMAGE_Y / 3)
 
 
 def create_galaxy_image(grid):
-    output = Image.new('RGBA', (int(TILE_IMAGE_X * (6 * 0.75 + 1)), TILE_IMAGE_Y * 7), (0, 0, 0, 1))
+    output = Image.new('RGBA',
+                       (int(TILE_IMAGE_X * (6 * 0.75 + 1)), TILE_IMAGE_Y * 7),
+                       (0, 0, 0, 0 ))
+    txt = Image.new('RGBA',
+                     (int(TILE_IMAGE_X * (6 * 0.75 + 1)), TILE_IMAGE_Y * 7),
+                     (255, 255, 255, 0))
+    d = ImageDraw.Draw(txt)
+    font = ImageFont.truetype(FONT_PATH, size=TILE_IMAGE_Y/3)
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             tile_image = get_tile_image(grid[i][j])
             if (tile_image is None):
                 continue
-            output.paste(tile_image,
-                         calc_coordinates(i, j, TILE_IMAGE_X, TILE_IMAGE_Y, 3),
-                         tile_image)
+            coords = calc_coordinates(i, j, 3)
+            text_coords = calc_text_coords(i, j, 3)
+            output.paste(tile_image, coords, tile_image)
+            d.text(text_coords, str(grid[i][j]), font=font, fill=(255,255,255,255))
 
+    output = Image.alpha_composite(output, txt)
     return output
 
 if __name__ == "__main__":
