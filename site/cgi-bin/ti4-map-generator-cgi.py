f5934444fc23e6060f08851e31b14b7135bc8b1d
Derek Peterson <derekpeterson.dpp@gmail.com>
2018-07-04 15:16:38 -0400

    added seed/weight controls to site

--- site/cgi-bin/ti4-map-generator-cgi.py
+++ site/cgi-bin/ti4-map-generator-cgi.py
@@ -31,10 +31,19 @@ def generate_galaxy(args):
     id = random_string(6)
     galaxy_json_filename = "galaxy_%s.json" % id
     galaxy_png_filename = "galaxy_%s.png" % id
+
+    if "seed" in args:
+        seed = int(args["seed"].value)
+    else:
+        random.seed()
+        seed = random.randint(1000000,9999999)
+
     cmd = ["./ti4-map-generator",
            "-t", "tiles.json",
            "-o", os.path.join(GENERATED_DIR, galaxy_json_filename),
-           "-p", str(n_players)]
+           "-p", str(n_players),
+           "-s", str(seed)]
+
     if "race_selection_method" in args:
         if args["race_selection_method"].value == "random":
             cmd += ["--random_homes"]
@@ -59,6 +68,14 @@ def generate_galaxy(args):
     elif args["winnu_clear_path_to_mecatol"].value == "false":
         cmd += ["--winnu_have_clear_path_to_mecatol", "0"]
 
+    if "resource_weight" in args:
+        cmd += ["--resource_weight", str(float(args["resource_weight"].value))]
+    if "influence_weight" in args:
+        cmd += ["--influence_weight", str(float(args["influence_weight"].value))]
+    if "tech_weight" in args:
+        cmd += ["--tech_weight", str(float(args["tech_weight"].value))]
+
+
     p = subprocess.Popen(cmd,
         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     p.wait()
@@ -69,7 +86,7 @@ def generate_galaxy(args):
     draw_galaxy.create_galaxy_image(
         os.path.join(GENERATED_DIR, galaxy_json_filename),
         os.path.join(GENERATED_DIR, galaxy_png_filename))
-    return galaxy_png_filename
+    return galaxy_png_filename, seed
 
 
 def return_image(image):
@@ -83,8 +100,9 @@ def return_image(image):
 def return_main_page(args):
     print("Content-Type: text/html;charset=utf-8\n")
 
-    galaxy_img_name = generate_galaxy(args)
+    galaxy_img_name, seed = generate_galaxy(args)
 
+    print('<div align="right">Seed: %d</div><br>' % seed)
     print('<img src="./cgi-bin/ti4-map-generator-cgi.py?image=%s"/>' % galaxy_img_name)
 
 cgitb.enable()
