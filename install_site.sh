afbad8396a2c8f09f284451d22427d8cb5a8032e
Derek Peterson <derekpeterson.dpp@gmail.com>
2018-07-04 09:43:58 -0400

    added an install file
    builds generator cpp program and copies files to the apache install
    directory

---
+++ install_site.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+INSTALL_SITE_PATH=/var/www/html/
+
+cmake ./
+make 
+rsync ti4-map-generator site/cgi-bin
+rsync -avzP site/ $INSTALL_SITE_PATH
+mkdir -p $INSTALL_SITE_PATH/generated
+chown www-data $INSTALL_SITE_PATH
